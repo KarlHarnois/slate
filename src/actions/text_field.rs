0d61f49868940a39ee3d5423e1aa68209231f7d4
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2025-10-26 16:10:03 -0400

    Extract text input helpers

--- src/actions/text_field.rs
+++ src/actions/text_field.rs
@@ -11,60 +11,32 @@ pub struct MoveRightInTextField;
 
 impl Action for InsertChar {
     fn apply(self: Box<Self>, state: &mut AppState) {
-        let Some(modal) = state.modal.as_mut() else {
-            return;
+        if let Some(modal) = state.modal.as_mut() {
+            modal.text.insert(self.char);
         };
-
-        modal.text.insert(self.char);
     }
 }
 
 impl Action for DeleteChar {
     fn apply(self: Box<Self>, state: &mut AppState) {
-        let Some(modal) = state.modal.as_mut() else {
-            return;
+        if let Some(modal) = state.modal.as_mut() {
+            modal.text.delete_char();
         };
-
-        if modal.text.cursor == 0 {
-            return;
-        }
-        let before_char_to_delete =
-            modal.text.value.chars().take(modal.text.cursor - 1);
-
-        let after_char_to_delete =
-            modal.text.value.chars().skip(modal.text.cursor);
-
-        modal.text.value =
-            before_char_to_delete.chain(after_char_to_delete).collect();
-
-        modal.text.cursor -= 1;
     }
 }
 
 impl Action for MoveLeftInTextField {
     fn apply(self: Box<Self>, state: &mut AppState) {
-        let Some(modal) = state.modal.as_mut() else {
-            return;
+        if let Some(modal) = state.modal.as_mut() {
+            modal.text.move_cursor_left();
         };
-
-        if modal.text.cursor == 0 {
-            return;
-        };
-
-        modal.text.cursor -= 1;
     }
 }
 
 impl Action for MoveRightInTextField {
     fn apply(self: Box<Self>, state: &mut AppState) {
-        let Some(modal) = state.modal.as_mut() else {
-            return;
+        if let Some(modal) = state.modal.as_mut() {
+            modal.text.move_cursor_right()
         };
-
-        if modal.text.cursor == modal.text.value.len() {
-            return;
-        };
-
-        modal.text.cursor += 1;
     }
 }
--- src/states/text_input.rs
+++ src/states/text_input.rs
@@ -16,4 +16,101 @@ impl TextInput {
         self.value.insert(self.cursor, char);
         self.cursor += 1;
     }
+
+    pub fn delete_char(&mut self) {
+        if self.cursor == 0 {
+            return;
+        }
+
+        let before_char_to_delete = self.value.chars().take(self.cursor - 1);
+        let after_char_to_delete = self.value.chars().skip(self.cursor);
+
+        self.value =
+            before_char_to_delete.chain(after_char_to_delete).collect();
+
+        self.cursor -= 1;
+    }
+
+    pub fn move_cursor_left(&mut self) {
+        if self.cursor == 0 {
+            return;
+        };
+        self.cursor -= 1;
+    }
+
+    pub fn move_cursor_right(&mut self) {
+        if self.cursor == self.value.len() {
+            return;
+        }
+        self.cursor += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::states::TextInput;
+
+    #[test]
+    fn insert() {
+        let mut input = TextInput {
+            value: "test".to_string(),
+            cursor: 2,
+        };
+
+        input.insert('a');
+
+        assert_eq!(
+            input.value,
+            "teast".to_string(),
+            "Char is inserted at the cursor"
+        );
+
+        assert_eq!(input.cursor, 3, "Cursor is moved");
+    }
+
+    #[test]
+    fn delete_char() {
+        let mut input = TextInput {
+            value: "test".to_string(),
+            cursor: 2,
+        };
+
+        input.delete_char();
+
+        assert_eq!(
+            input.value,
+            "tst".to_string(),
+            "Char is deleted at the cursor"
+        );
+
+        assert_eq!(input.cursor, 1, "Cursor is moved");
+    }
+
+    #[test]
+    fn move_cursor_left() {
+        let mut input = TextInput {
+            value: "test".to_string(),
+            cursor: 1,
+        };
+
+        input.move_cursor_left();
+        assert_eq!(input.cursor, 0, "Cursor is decremented");
+
+        input.move_cursor_left();
+        assert_eq!(input.cursor, 0, "Cursor cannot move pass minimum");
+    }
+
+    #[test]
+    fn move_cursor_right() {
+        let mut input = TextInput {
+            value: "test".to_string(),
+            cursor: 3,
+        };
+
+        input.move_cursor_right();
+        assert_eq!(input.cursor, 4, "Cursor is incremented");
+
+        input.move_cursor_right();
+        assert_eq!(input.cursor, 4, "Cursor cannot move pass maximum");
+    }
 }
